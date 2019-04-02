43ccd3e6ce38df75ac399c0850bbd540b31ce8fa
JasonWatts <jawatts@westmont.edu>
2019-04-02 15:32:38 -0700

    removed unneeded models

--- db/schema.rb
+++ db/schema.rb
@@ -12,23 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_12_080410) do
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tasks", force: :cascade do |t|
     t.string "assignee"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "tasks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
